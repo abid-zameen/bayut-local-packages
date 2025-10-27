24d2be9339b69e928d56319e022448d4b22aa0c2
Abid Hussain <56594568+abid-zameen@users.noreply.github.com>
2025-10-27 15:39:09 +0500

    Making few changes in Marquee label to support

--- MarqueeLabel/Package.swift
+++ MarqueeLabel/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MarqueeLabel",
     platforms: [
-        .iOS(.v12),
-        .tvOS(.v12)
+        .iOS(.v12)        
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.

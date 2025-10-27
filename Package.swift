ab4dec5504268d10cb6eea4d6a687f6964e47511
Abid Hussain <56594568+abid-zameen@users.noreply.github.com>
2025-10-27 15:30:49 +0500

    Fixing hosting package swift file.

--- Package.swift
+++ Package.swift
@@ -3,10 +3,36 @@ import PackageDescription
 
 let package = Package(
     name: "BayutLocalPackages",
-    products: [],
-    dependencies: [
-        .package(path: "./NetworkKit"),
-        .package(path: "./AnalyticsKit")
+    platforms: [.iOS(.v13)],
+    products: [
+        // A dummy product to make the package valid
+        .library(name: "BayutLocalPackages", targets: ["BayutLocalPackages"])
     ],
-    targets: []
+    dependencies: [
+        .package(path: "./MarqueeLabel"),
+        .package(path: "./ImageSlideshow"),
+        .package(path: "./Presentr"),
+        .package(path: "./RangeSeekSlider"),
+        .package(path: "./ReadMoreTextView"),
+        .package(path: "./TTTAttributedLabel"),
+        .package(path: "./SlideMenuController"),
+        .package(path: "./DZNWebViewController")
+    ],
+    targets: [
+        .target(
+            name: "BayutLocalPackages",
+            dependencies: [
+                "MarqueeLabel",
+                "ImageSlideshow",
+                "Presentr",
+                "RangeSeekSlider",
+                "ReadMoreTextView",
+                "TTTAttributedLabel",
+                "SlideMenuControllerSwift",
+                "DZNWebViewController"
+            ],
+            path: "Sources/BayutLocalPackages"
+        )
+    ]
 )
+
---
+++ Sources/BayutLocalPackages/Placeholder.swift
@@ -0,0 +1,2 @@
+// Placeholder file to satisfy SwiftPM target requirements.
+public struct BayutLocalPackagesPlaceholder {}
